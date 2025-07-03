83154adbf7264779ffe11ffe913902b5761525c3
avreetkaur84 <avreetkaur084@gmail.com>
2025-07-03 13:53:23 +0530

    updated

--- model/main.py
+++ model/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from model.predictor import predict_transcript
+from predictor import predict_transcript
 from fastapi.middleware.cors import CORSMiddleware
 
 import nltk
